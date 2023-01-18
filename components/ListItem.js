765985105cae9fab61e83aaa27c2dcc1409eceb7
maijuhimberg <maiju.himberg@metropolia.fi>
2023-01-18 10:34:53 +0200

    first app exercise done

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import {Image, View, Text, TouchableOpacity,
-} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Image} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
 import PropTypes from 'prop-types';
-import { uploadsUrl } from '../utils/variables';
 
 const ListItem = ({singleMedia}) => {
-  const item = singleMedia;
-  return (
-    <TouchableOpacity>
-      <Image
-        style={{width: 100, height: 100}}
-        source={{uri: uploadsUrl + item.thumbnails?.w160}}
-      ></Image>
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.description}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-ListItem.propTypes = {
-  singleMedia: PropTypes.object,
-};
-
-export default ListItem;
+    const item = singleMedia;
+    return (
+      <TouchableOpacity>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: item.thumbnails.w160}}
+        ></Image>
+        <View>
+          <Text>{item.title}</Text>
+          <Text>{item.description}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  
+  ListItem.propTypes = {
+    singleMedia: PropTypes.object,
+  };
+  
+  export default ListItem;
