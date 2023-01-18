82c0338aca5192ce04b8dfa16da712329e849d44
maijuhimberg <maiju.himberg@metropolia.fi>
2023-01-18 14:32:01 +0200

    AJAX task B

--- components/List.js
+++ components/List.js
@@ -1,9 +1,9 @@
 import {FlatList, StyleSheet} from 'react-native';
 import {useEffect, useState} from 'react';
 import ListItem from './ListItem';
+import {baseUrl} from '../utils/variables';
 
 
- 
   const List = () => {
     const url =
     'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
@@ -11,9 +11,16 @@ import ListItem from './ListItem';
 
   const loadMedia = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(baseUrl + 'media');
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (file) => {
+          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
+          return await fileResponse.json();
+        })
+      );
+
+      setMediaArray(media);
     } catch (error) {
       console.error('List, loadMedia', error);
     }
@@ -31,5 +38,5 @@ import ListItem from './ListItem';
       />
     );
   };
-  
+
   export default List;
